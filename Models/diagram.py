0db14c1e23d95421f4006491bd634b7bd374e79d
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-02-01 15:21:25 -0500

    Added blank file for Diagram class

---
+++ Models/diagram.py
@@ -0,0 +1,5 @@
+class Diagram:
+    # Katie Dowlin: List to keep track of which attribute names have been used.
+    used_attribute_names = list()
+    # Katie Dowlin: List to keep track of which class names have been used.
+    used_class_names = list()
