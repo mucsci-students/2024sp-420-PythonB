1d75e7aca7e505e4521fb9cdaf8554cc8d401591
Danish Zubari <danishkamran2000@gmail.com>
2024-02-06 11:51:18 -0500

    It's a classes file.

--- Models/addclass.py
+++ Models/addclass.py
@@ -1,28 +1,46 @@
 # Primary: Danish
 # secondary : Zhang
-#updated_version Feb 5,2024
-from namechecker import *
+# updated_version Feb 5,2024
+from Models.diagram import Diagram
+# from relationship import Relationship
 
 
 class UMLClass:
     def __init__(self):
         # Danish: create an empty dictionary
         self.classes = {}
+        # self.relationships = relationships
+
         # create object from diagram class
         self.diagram = Diagram()
+
     # Danish: I'm writing this function to add a class name
 
     def add_class(self, name):
         # Danish: If the class name doesn't exist it will print the "class name added successfully"
-        if self.diagram.class_name_checker(name):
+        """if name in self.classes:
+            print(f"{name} already exist")
+            return name
+            # Danish: If the class name doesn't exist it will print the "class name added successfully"
+        if name not in self.classes:
+            if len(name) == 0:
+                print("invalid name")
+                return name
+            else:
+                self.classes[name] = {}
+                print(f"{name} added successfully")
+                return name
+        """
+        if self.diagram.name_checker(name):
             self.classes[name] = []
             print(f"{name} added successfully")
             return name
-
     # Danish: I'm writing this function to delete a class name
+
     def delete_class(self, name):
         # Danish: if class is exist then it'll delete and display "class name deleted successfully
         if name in self.classes:
+            # self.relationship.delete_classes(name)
             del self.classes[name]
             print(f"{name} deleted successfully")
             return name
@@ -41,7 +59,8 @@ class UMLClass:
         # Danish: If the class exists, it will be renamed to the new name,
         # and the message "Class name renamed to new_name successfully" will be displayed
         # Danish: If the class name exists, it will remove the old name and assign it a new name.
-        if self.diagram.class_name_checker(newname):
+        # if  self.diagram.class_name_checker(name):
+        if name in self.classes and newname not in self.classes:
             self.classes[newname] = self.classes.pop(name)
             print(f"{name} renamed to {newname} successfully")
             return newname
@@ -55,9 +74,11 @@ class UMLClass:
 
 
 # for my testing
-"""
+
 umlclass = UMLClass()
 umlclass.add_class('Student')
+umlclass.add_class('1hello')
+umlclass.add_class('dani')
 umlclass.add_class('MID')
 umlclass.add_class('')
 umlclass.add_class('assert')
@@ -67,10 +88,9 @@ umlclass.add_class('Student')
 umlclass.add_class('Student')
 umlclass.add_class(' ')
 print("all classes after adding ", umlclass.list_class())
-umlclass.rename_class('MID','Student1')
-umlclass.rename_class('Grades','Student1')
-umlclass.rename_class('Ali','Danish')
+umlclass.rename_class('MID', 'Student1')
+umlclass.rename_class('Grades', 'Student1')
+umlclass.rename_class('Ali', 'Danish')
 print("all classes after rename ", umlclass.list_class())
 umlclass.delete_class('Student')
 print("all classes after delete ", umlclass.list_class())
-"""
--- Test/classTest.py
+++ Test/classTest.py
@@ -1,5 +1,5 @@
-import unittest
 from Models.addclass import UMLClass
+import unittest
 
 
 class TestClass(unittest.TestCase):
