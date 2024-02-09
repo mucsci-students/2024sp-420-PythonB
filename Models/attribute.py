181c373c1737c2e076df716747e5b8d3ac82c170
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-02-09 11:09:06 -0500

    Added comments

--- Models/attribute.py
+++ Models/attribute.py
@@ -13,39 +13,51 @@ class Attributes:
                 diagram.classes[class_name].append(name)
                 print(name + " Added Successfully")
                 return name
+            # Katie Dowlin: If the class you want to add the attribute to doesn't exist.
             else:
                 print("Add Failed- " + class_name + " Doesn't Exist")
                 return name
-
+        # Katie Dowlin: If the name is invalid.
         else:
             print("Add Failed- " + name + " Is Invalid")
             return name
 
     def delete_attribute(self, name, class_name):
+        # Katie Dowlin: Check if class exists.
         if class_name in diagram.classes.keys():
+            # Katie Dowlin: Check if an attribute with that name exists in that class.
             if name in diagram.classes[class_name]:
                 diagram.classes[class_name].remove(name)
                 print(name + " Deleted Successfully")
+            # Katie Dowlin: If there is no attribute with that name in that class.
             else:
                 print("Delete Failed-" + name + " Doesn't Exist")
                 return name
+        # Katie Dowlin: If the class doesn't exist.
         else:
             print("Delete Failed- " + class_name + " Doesn't Exist")
             return name
 
+
     def rename_attribute(self, old_name, new_name, class_name):
+        # Katie Dowlin: Check if class exists.
         if class_name in diagram.classes.keys():
+            # Katie Dowlin: Check if old name is an attribute in that class.
             if old_name in diagram.classes[class_name]:
+                # Katie Dowlin: Check if new name is valid.
                 if diagram.name_checker(self, new_name):
                     diagram.classes[class_name].remove(old_name)
                     diagram.classes[class_name].append(new_name)
                     print(old_name + " Has Been Renamed To " + new_name)
                     return new_name
+                # Katie Dowlin: If the new name is invalid.
                 else:
                     print("Rename Failed- " + new_name + " Is Invalid")
                     return old_name
+            # Katie Dowlin: If the old name does not exist.
             else:
                 print("Rename Failed- " + old_name + "Doesn't Exist")
+        # Katie Dowlin: If the class doesn't exist.
         else:
             print("Rename Failed- " + class_name + "Doesn't Exist")
             return old_name
