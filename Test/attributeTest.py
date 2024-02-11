f903dd9eb617f301f598d096b92f00ade0f850fa
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-02-11 10:55:41 -0500

    Attribute test file- still working on debugging

--- Test/attributeTest.py
+++ Test/attributeTest.py
@@ -2,49 +2,47 @@ import unittest
 from Models.attribute import Attributes
 from Models.addclass import UMLClass
 from Models.diagram import Diagram
+
+
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.attributes = Attributes()
-        self.diagram = Diagram()
-        self.umlclass = UMLClass()
-        self.umlclass.add_class("Students")
-        self.diagram.classes["Students"].append("student1")
-
+        self.attributes = Attributes(self.classes)
+        self.classes = UMLClass()
+        #self.umlclass.add_class("Students")
+        diagram.classes.update({"Students": []})
+        diagram.classes["Students"].append("student1")
+        diagram.classes["Students"].append("student2")
 
     def test_add_attribute(self):
-        # Katie Dowlin: Class exists. Expected return value: "Katie"
-        result = self.attributes.add_attribute("Katie", "Student")
-        self.assertEqual(result, "Katie")
-        # Katie Dowlin: Valid name, class_name doesn't exist.
-        result9 = self.attributes.add_attribute("name", "Imaginary")
-        self.assertEqual(result9, "name")
-
+        # Katie Dowlin: Class exists. Add should be successful, so name should be in the list of attributes
+        # in that class.
+        self.attributes.add_attribute("Katie", "Students")
+        self.assertIn(self.diagram.classes["Students"], "Katie")
+        # Katie Dowlin: Class doesn't exist. Add should fail, so should receive a value error.
+        self.assertRaises(ValueError, self.attributes.add_attribute, "Katie", "Class")
 
     def test_delete_attribute(self):
-        # Katie Dowlin: Both name and class_name exist.
-        result = self.attributes.delete_attribute("attribute2", "Class")
-        self.assertEqual(result, "attribute2")
-        # Katie Dowlin: Name doesn't exist, class_name exists.
-        result2 = self.attributes.delete_attribute("hello", "Class")
-        self.assertEqual(result2, "hello")
-        # Katie Dowlin: Class_name doesn't exist.
-        result3 = self.attributes.delete_attribute("attribute", "Imaginary")
-
+        # Katie Dowlin: Attribute and class both exist. Delete should be successful, so name should not be in
+        # the list of attributes in that class.
+        self.attributes.delete_attribute("student1", "Students")
+        self.assertNotIn(self.diagram.classes["Students"], "student1")
+        # Katie Dowlin: Class exists, but attribute doesn't. Delete should fail, so should receive a value error.
+        self.assertRaises(ValueError, self.attributes.delete_attribute, "student3", "Students")
+        # Katie Dowlin: Class doesn't exist. Delete should fail, so should receive a value error.
+        self.assertRaises(ValueError, self.attributes.delete_attribute, "student2", "Class")
 
     def test_rename_attribute(self):
-        # Katie Dowlin: Class_name exists, old_name exists, new_name is valid
-        result = self.attributes.rename_attribute("attribute", "attribute1", "Class")
-        self.assertEqual(result, "attribute1")
-        # Katie Dowlin: Class_name exists, old_name exists, new_name is not valid
-        result2 = self.attributes.rename_attribute("attribute3", "for", "Class")
-        self.assertEqual(result2, "attribute3")
-        # Katie Dowlin: Class_name exists, old_name doesn't exist
-        result3 = self.attributes.rename_attribute("attribute5", "attribute6", "Class")
-        self.assertEqual(result3, "attribute5")
-        # Katie Dowlin: Class_name doesn't exist
-        result4 = self.attributes.rename_attribute("hello", "goodbye", "Imaginary")
-        self.assertEqual(result4, "hello")
+        print(self.diagram.classes["Students"])
+        # Katie Dowlin: Class and old name both exist. Rename should be successful, so the new name should be
+        # in the list of attributes in that class and the old name should no longer be in the list.
+        self.attributes.rename_attribute("student2", "student1", "Students")
+        self.assertIn(self.diagram.classes["Students"], "student1")
+        self.assertNotIn(self.diagram.classes["Students"], student2)
+        # Katie Dowlin: Class exists, but old name doesn't. Rename should fail, so should receive a value error.
+        self.assertRaises(ValueError, self.attributes.rename_attribute, "student3", "student2", "Students")
+        # Katie Dowlin: Class doesn't exist. Rename should fail, so should receive a value error.
+        self.assertRaises(ValueError, self.attributes.rename_attribute, "student1", "student2", "Class")
 
 
 if __name__ == '__main__':
