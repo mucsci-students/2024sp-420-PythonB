72c9fd45749921fcfcaccb84e3500becf2f3a22a
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-02-22 14:57:54 -0500

    Finished converting all tests to pytest

--- Test/test_classadd.py
+++ Test/test_classadd.py
@@ -1,5 +1,8 @@
 from Models.classadd import UMLClass
 from Models.diagram import Diagram
+from Models.attribute import Fields
+from Models.attribute import Methods
+from Models.attribute import Parameters
 import pytest
 
 
@@ -18,7 +21,6 @@ def test_add_duplicate_class():
         test_class.add_class('Duplicate')
 
 
-# test for rename class
 def test_rename_class():
     test_diagram = Diagram()
     test_class = UMLClass(test_diagram)
@@ -27,14 +29,14 @@ def test_rename_class():
         test_class.rename_class('Classes', 'Schools')
 
 
-def test_rename_class_not_exist(self):
+def test_rename_class_not_exist():
     test_diagram = Diagram()
     test_class = UMLClass(test_diagram)
     with pytest.raises(TypeError):
         test_class.rename_class('Employees', 'Managers')
 
 
-def test_rename_to_duplicate_name(self):
+def test_rename_to_duplicate_name():
     test_diagram = Diagram()
     test_class = UMLClass(test_diagram)
     test_class.add_class('Class1')
@@ -43,7 +45,7 @@ def test_rename_to_duplicate_name(self):
         test_class.rename_class('Class1', 'Class2')
 
 
-def test_delete_class(self):
+def test_delete_class():
     test_diagram = Diagram()
     test_class = UMLClass(test_diagram)
     test_class.add_class('Managers')
@@ -51,27 +53,44 @@ def test_delete_class(self):
         test_class.delete_class('Managers')
 
 
-def test_delete_class_not_exist(self):
+def test_delete_class_not_exist():
     test_diagram = Diagram()
     test_class = UMLClass(test_diagram)
     with pytest.raises(TypeError):
         test_class.delete_class('Grade')
 
 
-def test_delete_classrelationship(self):
-    self.classname.add_class('Teacher')
-    result = self.classname.delete_class('Teacher')
-    self.assertEqual(result, 'Teacher')
+def test_listclasses():
+    test_diagram = Diagram()
+    test_class = UMLClass(test_diagram)
+    test_class.add_class('Students')
+    test_class.add_class('MID')
+    test_class.add_class('Grade')
+    expected = {'Students': [], 'MID': [], 'Grade': []}
+    assert test_class.list_classes() == expected
 
 
-def test_listclasses(self):
-    self.classname.add_class('Students')
-    self.classname.add_class('MID')
-    self.classname.add_class('Grade')
-    exist_class = self.classname.list_class()
-    expected = {
-        "Students": [],
-        "MID": [],
-        "Grade": []
-    }
-    self.assertDictEqual(exist_class, expected)
+def test_list_class():
+    test_diagram = Diagram()
+    test_class = UMLClass(test_diagram)
+    test_field = Fields(test_diagram, test_class)
+    test_method = Methods(test_diagram, test_class)
+    test_parameter = Parameters(test_method)
+    test_class.add_class('CSCI420')
+    test_field.add_field('Katie', 'CSCI420')
+    test_field.add_field('Danish', 'CSCI420')
+    test_field.add_field('Jillian', 'CSCI420')
+    test_field.add_field('Patrick', 'CSCI420')
+    test_field.add_field('Zhang', 'CSCI420')
+    test_method.add_method('Project', 'CSCI420')
+    test_parameter.add_parameter('due_date', 'Project')
+    expected = {'CSCI420': {'Fields': ['Katie', 'Danish', 'Jillian', 'Patrick', 'Zhang'],
+                            'Methods': [{'Project': {'Parameters': ['due_date']}}]}}
+    assert test_class.list_class() == expected
+
+def test_list_class_no_methods_fields_params():
+    test_diagram = Diagram()
+    test_class = UMLClass(test_diagram)
+    test_class.add_class('CSCI420')
+    expected = {'CSCI420': {'Fields': [], 'Methods': []}}
+    assert test_class.list_class() == expected
