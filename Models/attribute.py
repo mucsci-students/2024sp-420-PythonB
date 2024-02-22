d4531ce8c9c92c0734d517b3de6f5a692e03d375
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-02-22 16:22:29 -0500

    Fixed constructor parameters

--- Models/attribute.py
+++ Models/attribute.py
@@ -2,15 +2,15 @@ from Models.diagram import Diagram
 
 
 class Fields:
-    def __init__(self, diagram):
-        self.diagram = diagram
+    def __init__(self, diagram_class):
+        self.diagram_class = diagram_class
 
 
 class Methods:
-    def __init__(self, diagram):
-        self.diagram = diagram
+    def __init__(self, diagram_class):
+        self.diagram_class = diagram_class
 
 
 class Parameters:
-    def __init__(self, diagram):
-        self.diagram = diagram
+    def __init__(self, method):
+        self.method = method
