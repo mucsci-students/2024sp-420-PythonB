5c237841d2f59d07dbc7105e749bc2a5f4c08bf7
Peter <frdmanp@gmail.com>
2024-03-13 10:19:40 -0400

    Platform Specific build scripts (but they're nice)

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@ no_venv= [ -z "$VIRTUAL_ENV" ];
 env_name="venv"
 if $no_venv; then
     read -p "No venv activated; insert name of existing or new venv: " env_name
-    venv_exists= [ ! -d "$VENV_PATH" ]
-    if $venv_exists; then
+    no_venv_path= [ ! -d "$VENV_PATH" ]
+    if $no_venv_path; then
         python3 -m venv "$env_name"
     fi
 fi
 
 source "$PWD/$env_name/bin/activate"
 pip install "$PWD"
+clear
+python3 uml.py
+clear
+deactivate
