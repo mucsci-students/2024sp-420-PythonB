af115695d02b0211c218c8fe173dfe7cb5905ea1
Tim <timbmoser@gmail.com>
2024-03-15 16:02:32 -0400

    UML_Field Progress

--- Models/uml_field.py
+++ Models/uml_field.py
@@ -16,33 +16,39 @@ class UML_Field (UML_Named_Object):
         return self._type
     
 #===================================== Mutators =====================================#
-    def set_name (self, new_name:str) -> None:
+    def set_name(self, new_name:str) -> None:
         '''Mutator for this field's name'''
         self._name = new_name
 
-    def set_type (self, new_name:str) -> None:
+    def set_type(self, new_name:str) -> None:
         '''Mutator for this field's return type'''
         self._type = new_name
 
 #===================================== Operators =====================================#
-        def __eq__ (self, o) -> bool:
-            if self is o:
-                return True
-
-            if not isinstance(o, UML_Field):
-                return False
-            
-            if self._name != o._name:
-                return False
-            
-            if self._type != o._type:
-                return False
-            
+    def __eq__(self, o) -> bool:
+        if self is o:
             return True
-        
-        def __str__ (self) -> str:
-            '''Strings a field in the following form:
-                type
-                name'''
-            return self._type + '\n' + self._name
+
+        if not isinstance(o, UML_Field):
+            return False
+
+        if self._name != o._name:
+            return False
+
+        if self._type != o._type:
+            return False
+
+        return True
+    
+    def __hash__(self):
+        return hash(self._name) \
+            + hash(self._type)
+    
+    def __str__(self) -> str:
+        '''Strings a field in the following form:
+            type
+            name
+        '''
+        result = self._type + '\n' + self._name
+        return result
             
--- Test/fields_test.py
+++ Test/fields_test.py
@@ -10,18 +10,15 @@ def test_ctor_field():
     assert fld2._name == "field2"
     assert fld2._type == "void"
 
-fld1 = UML_Field("field1", "int")
-fld2 = UML_Field("field2", "str")
-fld3 = UML_Field("field3", "int")
-fld4 = UML_Field("field1", "str")
-fld5 = UML_Field("field1", "int")
-
 def test_get_name_field():
+    fld1 = UML_Field("field1", "int")
+    fld2 = UML_Field("field2", "str")
+    fld3 = UML_Field("field3", "int")
+    fld4 = UML_Field("field1", "str")
     name1 = fld1.get_name()
     name2 = fld2.get_name()
     name3 = fld3.get_name()
     name4 = fld4.get_name()
-    name5 = fld5.get_name()
     assert name1 == "field1"
     assert name1 == fld1._name
     assert name2 == "field2"
@@ -29,10 +26,13 @@ def test_get_name_field():
     assert name3 == "field3"
     assert name3 == fld3._name
     assert name4 == name1
-    assert name5 == name1
-    assert name5 == name4
 
 def test_get_type_field():
+    fld1 = UML_Field("field1", "int")
+    fld2 = UML_Field("field2", "str")
+    fld3 = UML_Field("field3", "int")
+    fld4 = UML_Field("field1", "str")
+    fld5 = UML_Field("field1", "int")
     type1 = fld1.get_type()
     type2 = fld2.get_type()
     type3 = fld3.get_type()
@@ -49,79 +49,54 @@ def test_get_type_field():
     assert type5 == type3
 
 def test_set_name_field():
+    fld1 = UML_Field("field1", "int")
+    fld2 = UML_Field("field2", "str")
+    fld3 = UML_Field("field3", "int")
     assert fld1._name == "field1"
     assert fld2._name == "field2"
     assert fld3._name == "field3"
-    assert fld4._name == "field1"
-    assert fld5._name == "field1"
-    fld1.set_name("Robert E. O. Speedwagon")
-    fld2.set_name("Joseph Joestar")
-    fld3.set_name("Dio")
-    fld4.set_name("Jotaro Kujo")
-    fld5.set_name("Star Platinum")
-    assert fld1._name == "Robert E. O. Speedwagon"
+    fld1.set_name("Dio")
+    fld2.set_name("Jotaro Kujo")
+    fld3.set_name("Star Platinum")
+    assert fld1._name == "Dio"
     assert fld1._name != "field1"
-    assert fld2._name == "Joseph Joestar"
+    assert fld2._name == "Jotaro Kujo"
     assert fld2._name != "field2"
-    assert fld3._name == "Dio"
+    assert fld3._name == "Star Platinum"
     assert fld3._name != "field3"
-    assert fld4._name == "Jotaro Kujo"
-    assert fld4._name != "field1"
-    assert fld5._name == "Star Platinum"
-    assert fld5._name != "field1"
-
-fld1.set_name("field1")
-fld2.set_name("field2")
-fld3.set_name("field3")
-fld4.set_name("field1")
-fld5.set_name("field1")
 
 def test_set_type_field():
+    fld1 = UML_Field("field1", "int")
+    fld2 = UML_Field("field2", "str")
+    fld3 = UML_Field("field3", "int")
     assert fld1._type == "int"
     assert fld2._type == "str"
     assert fld3._type == "int"
-    assert fld4._type == "str"
-    assert fld5._type == "int"
-    fld1.set_type("I'll always be there for you guys!")
-    fld2.set_type("Next you'll say...")
-    fld3.set_type("MUDA!")
-    fld4.set_type("Good Grief...")
-    fld5.set_type("ORA!")
-    assert fld1._type == "I'll always be there for you guys!"
+    fld1.set_type("MUDA!")
+    fld2.set_type("Good Grief...")
+    fld3.set_type("ORA!")
+    assert fld1._type == "MUDA!"
     assert fld1._type != "int"
-    assert fld2._type == "Next you'll say..."
+    assert fld2._type == "Good Grief..."
     assert fld2._type != "str"
-    assert fld3._type == "MUDA!"
+    assert fld3._type == "ORA!"
     assert fld3._type != "int"
-    assert fld4._type == "Good Grief..."
-    assert fld4._type != "str"
-    assert fld5._type == "ORA!"
-    assert fld5._type != "int"
-    
-fld1.set_type("int")
-fld2.set_type("str")
-fld3.set_type("int")
-fld4.set_type("str")
-fld5.set_type("int")
 
 def test_eq_field():
+    fld1 = UML_Field("field1", "int")
+    fld2 = UML_Field("field2", "str")
+    fld3 = UML_Field("field3", "int")
+    fld4 = UML_Field("field1", "str")
+    fld5 = UML_Field("field1", "int")
     assert fld1 == fld1
     assert fld1 != fld2
     assert fld1 != fld3
     assert fld1 != fld4
-    assert fld1 != fld5
-    assert fld2 == fld2
-    assert fld2 != fld3
-    assert fld2 != fld4
-    assert fld2 != fld5
-    assert fld3 == fld3
-    assert fld3 != fld4
-    assert fld3 != fld5
-    assert fld4 == fld4
-    assert fld4 != fld5
-    assert fld5 == fld5
+    assert fld1 == fld5
 
 def test_str_field():
+    fld1 = UML_Field("field1", "int")
     result1 = "int\nfield1"
-    
-    assert str(fld1) == result1
+    test1 = str(fld1)
+    print(test1)
+    assert test1 == result1
