79d5ca3ba0a2d58a4a9b21aff7c74f90a0d521ec
Marshall Feng <fengdier2@126.com>
2024-03-18 12:06:45 -0400

    Finish save_load for class

--- src/Models/uml_save_load.py
+++ src/Models/uml_save_load.py
@@ -15,19 +15,32 @@ class UML_Save_Visitor(UML_Visitor):
     def visit_diagram(self, uml_diagram):
         raise NotImplementedError
     
-    def visit_class(self, uml_class):
-        raise NotImplementedError
+    def visit_class(self, uml_class: UML_Class):
+        return {
+                'name'    : uml_class.get_name(),
+                'fields'  : [self.visit_field(uml_field=field) for field in uml_class.get_fields()],
+                'methods' : [self.visit_method(uml_method=method) for method in uml_class.get_methods()],
+                'position': {'x': 0, 'y': 0} #TODO: position to be defined
+               }
     
     def visit_field(self, uml_field: UML_Field):
-        return {'name': uml_field.get_name(), 'type': uml_field.get_type()}
+        return {
+                'name': uml_field.get_name(),
+                'type': uml_field.get_type()
+               }
         
     def visit_method(self, uml_method: UML_Method):
-        return {'name'       : uml_method.get_name(),\
-                'return_type': uml_method.get_ret(), \
-                'params'     : [self.visit_param(uml_param=param) for param in uml_method.get_params()]}
+        return {
+                'name'       : uml_method.get_name(),
+                'return_type': uml_method.get_ret(),
+                'params'     : [self.visit_param(uml_param=param) for param in uml_method.get_params()]
+               }
     
     def visit_param(self, uml_param: UML_Param):
-        return {'name': uml_param.get_name(), 'type': 'undefined'} #TODO: 'type' to be defined
+        return {
+                'name': uml_param.get_name(),
+                'type': 'undefined' #TODO: 'type' to be defined
+               }
         
     
     def visit_relation(self, uml_relation):
@@ -56,3 +69,12 @@ def load_param(obj):
 def load_method(obj):
     params = [load_param(param).get_name() for param in obj['params']] #TODO: type to be defined
     return UML_Method(obj['name'], obj['return_type'], *params)
+
+def load_class(obj):
+    uml_class = UML_Class(obj['name'])
+    for field in obj['fields']:
+        uml_class._fields.append(load_field(field)) #TODO: add checks(add_field)
+    for method in obj['methods']:
+        uml_class._methods.append(load_method(method)) #TODO: add checks(add_method)
+    #TODO: position to be defined
+    return uml_class
--- src/tests/save_load_test.py
+++ src/tests/save_load_test.py
@@ -2,6 +2,7 @@ from Models.uml_save_load import UML_Save_Visitor, encode_json, decode_json
 from Models.uml_save_load import load_field
 from Models.uml_save_load import load_param
 from Models.uml_save_load import load_method
+from Models.uml_save_load import load_class
 from Models.uml_diagram import UML_Diagram
 from Models.uml_class import UML_Class
 from Models.uml_field import UML_Field
@@ -57,3 +58,88 @@ def test_save_load_method():
     loaded_mth2 = load_method(content)
     assert mth2 is not loaded_mth2
     assert mth2 == loaded_mth2
+
+    mth3 = UML_Method('Method2', 'void', 'param1', 'param2')
+    #save 'Method3'
+    obj = mth3.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Method3'
+    loaded_mth3 = load_method(content)
+    assert mth3 is not loaded_mth3
+    assert mth3 == loaded_mth3
+
+def test_save_load_class():
+    save_visitor = UML_Save_Visitor()
+
+    cls1 = UML_Class('Class1')
+    #save 'Class1'
+    obj = cls1.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Class1'
+    loaded_cls1 = load_class(content)
+    assert cls1 is not loaded_cls1
+    assert cls1 == loaded_cls1
+
+    cls2 = UML_Class('Class2')
+    cls2.add_field('Field1', 'string')
+    #save 'Class2'
+    obj = cls2.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Class2'
+    loaded_cls2 = load_class(content)
+    assert cls2 is not loaded_cls2
+    assert cls2 == loaded_cls2
+
+    cls3 = UML_Class('Class3')
+    cls3.add_field('Field1', 'string')
+    cls3.add_field('Field2', 'void')
+    #save 'Class3'
+    obj = cls3.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Class3'
+    loaded_cls3 = load_class(content)
+    assert cls3 is not loaded_cls3
+    assert cls3 == loaded_cls3
+
+    cls4 = UML_Class('Class4')
+    cls4.add_method('Method1', 'string')
+    #save 'Class4'
+    obj = cls4.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Class4'
+    loaded_cls4 = load_class(content)
+    assert cls4 is not loaded_cls4
+    assert cls4 == loaded_cls4
+
+    cls5 = UML_Class('Class5')
+    cls5.add_method('Method1', 'string')
+    cls5.add_method('Method2', 'void', 'Param1')
+    cls5.add_method('Method3', 'int', 'Param1', 'Param2')
+    #save 'Class5'
+    obj = cls5.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Class5'
+    loaded_cls5 = load_class(content)
+    assert cls5 is not loaded_cls5
+    assert cls5 == loaded_cls5
+
+    cls6 = UML_Class('Class6')
+    cls6.add_field('Field1', 'string')
+    cls6.add_field('Field2', 'void')
+    cls6.add_method('Method1', 'string')
+    cls6.add_method('Method2', 'void', 'Param1')
+    cls6.add_method('Method3', 'int', 'Param1', 'Param2')
+    #save 'Class6'
+    obj = cls6.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Class6'
+    loaded_cls6 = load_class(content)
+    assert cls6 is not loaded_cls6
+    assert cls6 == loaded_cls6
