9c17de0c6ab7541c449dea10e55e5a68d1aa36c6
Marshall Feng <fengdier2@126.com>
2024-03-18 00:06:23 -0400

    Finish save/load for method

--- src/Models/uml_save_load.py
+++ src/Models/uml_save_load.py
@@ -19,34 +19,40 @@ class UML_Save_Visitor(UML_Visitor):
         raise NotImplementedError
     
     def visit_field(self, uml_field: UML_Field):
-        obj = {'name': uml_field.get_name(), 'type': uml_field.get_type()}
-        try:
-            content = json.dumps(obj=obj)
-        except Exception as e:
-            raise e
-        return content
-    
-    def visit_method(self, uml_method):
-        raise NotImplementedError
+        return {'name': uml_field.get_name(), 'type': uml_field.get_type()}
+        
+    def visit_method(self, uml_method: UML_Method):
+        return {'name'       : uml_method.get_name(),\
+                'return_type': uml_method.get_ret(), \
+                'params'     : [self.visit_param(uml_param=param) for param in uml_method.get_params()]}
     
     def visit_param(self, uml_param: UML_Param):
-        obj = {'name': uml_param.get_name(), 'type': 'undefined'} #TODO: 'type' to be defined
-        try:
-            content = json.dumps(obj=obj)
-        except Exception as e:
-            raise e
-        return content
+        return {'name': uml_param.get_name(), 'type': 'undefined'} #TODO: 'type' to be defined
+        
     
     def visit_relation(self, uml_relation):
         raise NotImplementedError
 
 ################################################################################
 
+def encode_json(obj):
+    try:
+        content = json.dumps(obj=obj)
+    except Exception as e:
+        raise e
+    return content
+
 def decode_json(content):
     return json.loads(content)
+
+################################################################################
 
 def load_field(obj):
     return UML_Field(name=obj['name'], type=obj['type'])
 
 def load_param(obj):
     return UML_Param(name=obj['name']) #TODO: type to be defined
+
+def load_method(obj):
+    params = [load_param(param).get_name() for param in obj['params']] #TODO: type to be defined
+    return UML_Method(obj['name'], obj['return_type'], *params)
--- src/tests/save_load_test.py
+++ src/tests/save_load_test.py
@@ -1,4 +1,7 @@
-from Models.uml_save_load import UML_Save_Visitor, decode_json, load_field, load_param
+from Models.uml_save_load import UML_Save_Visitor, encode_json, decode_json
+from Models.uml_save_load import load_field
+from Models.uml_save_load import load_param
+from Models.uml_save_load import load_method
 from Models.uml_diagram import UML_Diagram
 from Models.uml_class import UML_Class
 from Models.uml_field import UML_Field
@@ -11,10 +14,12 @@ def test_save_load_field():
 
     fld1 = UML_Field('Field1')
     #save 'Field1'
-    json_content = fld1.accept(save_visitor)
-    content = decode_json(json_content)
+    obj = fld1.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
     #load 'Field1'
     loaded_fld1 = load_field(content)
+    assert fld1 is not loaded_fld1
     assert fld1 == loaded_fld1
 
 def test_save_load_param():
@@ -22,8 +27,33 @@ def test_save_load_param():
 
     prm1 = UML_Param('Param1')
     #save 'Param1'
-    json_content = prm1.accept(save_visitor)
-    content = decode_json(json_content)
+    obj = prm1.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
     #load 'Param1'
     loaded_prm1 = load_param(content)
+    assert prm1 is not loaded_prm1
     assert prm1 == loaded_prm1
+
+def test_save_load_method():
+    save_visitor = UML_Save_Visitor()
+
+    mth1 = UML_Method('Method1', 'string')
+    #save 'Method1'
+    obj = mth1.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Method1'
+    loaded_mth1 = load_method(content)
+    assert mth1 is not loaded_mth1
+    assert mth1 == loaded_mth1
+
+    mth2 = UML_Method('Method2', 'void', 'param1')
+    #save 'Method2'
+    obj = mth2.accept(save_visitor)
+    json = encode_json(obj)
+    content = decode_json(json)
+    #load 'Method2'
+    loaded_mth2 = load_method(content)
+    assert mth2 is not loaded_mth2
+    assert mth2 == loaded_mth2
