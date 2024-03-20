75dfadba1c15d862c9748c6e48321f46c28c13de
AdamG-L <adamglicklynch1@gmail.com>
2024-03-19 20:16:59 -0400

    Moved autocompletion to the controller

--- src/Controllers/controller.py
+++ src/Controllers/controller.py
@@ -2,10 +2,13 @@ import sys
 from Controllers.cli_controller import CLI_Controller
 from Controllers.gui_controller import GUI_Controller
 from Views.cli_view import CLI_View
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import NestedCompleter
 class UML_Controller:
 
     def __init__(self):
         self._controller:CLI_Controller | GUI_Controller = self.__pick_controller()
+        self._autocompleter = self.setup_autocomplete()  
         
     
     def run(self):
@@ -16,4 +19,34 @@ class UML_Controller:
         if len(args) > 1 and str(args[1]).strip().lower() == 'cli':
             return CLI_Controller()
         return GUI_Controller()
+    
+    def setup_autocomplete(self):
+        return NestedCompleter.from_nested_dict({
+        'add': {
+            'class': None,
+            'field': None,
+            'method': None,
+            'relationship': None
+        },
+        'delete': {
+            'class': None,
+            'field': None,
+            'method': None,
+            'relationship': None
+        },
+        'rename':{
+            'class': None,
+            'field': None,
+            'method': None
+        },
+        'list':{
+            'class': None,
+            'classes': None,
+            'relationship': None,
+            'relationships': None
+        },
+        'save': None,
+        'load': None,
+        'exit': None
+        })  
     
