8d67a079ebf54ca5f2abef09538a8db4e3d8e7a5
almostTaklu <ach.ganga@outlook.com>
2024-03-19 23:40:33 -0400

    Small changes in undo_redo

--- src/Models/uml_undo_redo.py
+++ src/Models/uml_undo_redo.py
@@ -2,8 +2,8 @@ from Models.uml_diagram import UML_Diagram
 from Models.uml_save_load import diagram_to_json, json_to_diagram
 
 class UML_States:
-    def __init__(self) -> None:
-        self._states = []
+    def __init__(self, initial_diagram: UML_Diagram):
+        self._states = [diagram_to_json(initial_diagram)]
         self._current_state = 0
 
     def save_state(self, diagram: UML_Diagram):
@@ -18,14 +18,14 @@ class UML_States:
         """
         Undo the last change in the diagram, reverting to the previous state.
         """
-        if self._current_state > 1:
+        if self._current_state >= 1:
             self._current_state -= 1
-            return json_to_diagram(self._states[self._current_state - 1]) if self._states else None
+        return json_to_diagram(self._states[self._current_state])
 
     def redo(self):
         """
         Redoes the last undone change in the diagram, if any.
         """
-        if self._current_state < len(self._states):
+        if self._current_state < len(self._states) - 1:
             self._current_state += 1
-            return json_to_diagram(self._states[self._current_state - 1])
+        return json_to_diagram(self._states[self._current_state])
