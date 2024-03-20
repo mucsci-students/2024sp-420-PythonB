b3234383a05fca340acbb4c7af86c23ae2d808ac
almostTaklu <ach.ganga@outlook.com>
2024-03-20 14:40:18 -0400

    Refactor undo_redo test files

--- src/tests/undo_redo_test.py
+++ src/tests/undo_redo_test.py
@@ -1,45 +1,6 @@
 from Models.uml_undo_redo import UML_States
 from Models.uml_diagram import UML_Diagram
 
-def test_initial_state():
-    uml_diagram = UML_Diagram()
-    uml_states = UML_States(uml_diagram)
-
-    # The initial state should be empty
-    uml_diagram_afer_undo = uml_states.undo()
-    assert len(uml_diagram_afer_undo.get_all_classes()) == 0
-
-def test_save_and_undo():
-    uml_diagram = UML_Diagram()
-    uml_states = UML_States(uml_diagram)
-
-    initial_state = uml_states.undo()
-    assert len(initial_state.get_all_classes()) == 0
-
-    # Add a class and save the state
-    uml_diagram.add_class("Class1")
-    uml_states.save_state(uml_diagram)
-
-    # Undo to revert back to the initial state
-    uml_diagram_afer_undo = uml_states.undo()
-    assert len(uml_diagram_afer_undo.get_all_classes()) == 0
-
-def test_save_and_redo():
-    uml_diagram = UML_Diagram()
-    uml_states = UML_States(uml_diagram)
-
-   # Add a class and save the state
-    uml_diagram.add_class("Class1")
-    uml_states.save_state(uml_diagram)
-    
-    # Undo to revert back to the initial state
-    uml_states.undo()
-
-    # Redo to revert back to the state after the save
-    uml_diagram_after_redo = uml_states.redo()
-    assert len(uml_diagram_after_redo.get_all_classes()) == 1
-    assert uml_diagram_after_redo.get_all_classes()[0].get_name() == "Class1"
-
 def test_save_state():
     dgm0 = UML_Diagram()
 
