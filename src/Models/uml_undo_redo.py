340dc4b941e0d12b8d7a11c7ff72eb9c4cef8f02
Marshall Feng <fengdier2@126.com>
2024-03-19 23:46:53 -0400

    Small changes for better readability

--- src/Models/uml_undo_redo.py
+++ src/Models/uml_undo_redo.py
@@ -3,22 +3,28 @@ from Models.uml_save_load import diagram_to_json, json_to_diagram
 
 class UML_States:
     def __init__(self, initial_diagram: UML_Diagram):
-        self._states = [diagram_to_json(initial_diagram)]
-        self._current_state = 0
+        """
+        Initialize the UML_States with an initial state.
+        """
+        self._states = []
+        self._current_state = -1
+        self.save_state(initial_diagram)
 
     def save_state(self, diagram: UML_Diagram):
         """
         Save the current state of the diagram.
         """
+        # remove all states after the current state
         self._states = self._states[:self._current_state + 1]
-        self._states.append (diagram_to_json(diagram))
+        self._states.append(diagram_to_json(diagram))
         self._current_state += 1
 
     def undo(self):
         """
         Undo the last change in the diagram, reverting to the previous state.
         """
-        if self._current_state >= 1:
+        # The current state is after the first state
+        if self._current_state > 0:
             self._current_state -= 1
         return json_to_diagram(self._states[self._current_state])
 
@@ -26,6 +32,7 @@ class UML_States:
         """
         Redoes the last undone change in the diagram, if any.
         """
+        # The current state is before the last state
         if self._current_state < len(self._states) - 1:
             self._current_state += 1
         return json_to_diagram(self._states[self._current_state])
