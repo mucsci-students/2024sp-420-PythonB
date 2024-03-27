1e586ebf8a8981a443e56886093c71a1821c53f1
Marshall Feng <fengdier2@126.com>
2024-03-26 22:05:09 -0400

    Restructured controllers to make it easier to connect gui to controller

--- src/Controllers/cli_controller.py
+++ src/Controllers/cli_controller.py
@@ -4,25 +4,16 @@ from Models.uml_diagram import UML_Diagram
 from prompt_toolkit import prompt
 from Views.cli_view import CLI_View
 
-
-
 class CLI_Controller:
 
     def __init__(self):
         self._view = CLI_View()
     
-    
-    def update(self, parsed_input:list):
-        r_val = parsed_input[0](*parsed_input[1:])
-        if isinstance(r_val, str):
-            print(r_val) 
-        # return the Diagram if the function called returns a Diagram
-        if isinstance(r_val, UML_Diagram):
-            return r_val
-    
     def request_update(self):
          return prompt("Command: ", completer=self._view._completer).strip()
 
+    def draw(self, diagram: UML_Diagram):
+        pass
 
 #=========================CLI Specific Parseing=========================#  
     def parse_list_cmd(self, d:UML_Diagram, tokens:list[str]):
--- src/Controllers/gui_controller.py
+++ src/Controllers/gui_controller.py
@@ -1,16 +1,14 @@
+from Models.uml_diagram import UML_Diagram
 from Views.gui_view import GUI_View
 
 class GUI_Controller:
 
     def __init__(self):
-       self._gui_view = GUI_View()
-
-    def update (self):
-        while len(self._gui_view._commands) > 0:
-            cmd = self.request_update()
-            cmd[0](*cmd[1:])
+        self._gui_view = GUI_View()
 
     def request_update(self):
-        while len(self._gui_view._commands) == 0:
-            pass
-        return self._gui_view._commands.pop(0)
+        return self._gui_view.listen()
+    
+    def draw(self, diagram: UML_Diagram):
+        self._gui_view.clear()
+        self._gui_view.draw(diagram)
