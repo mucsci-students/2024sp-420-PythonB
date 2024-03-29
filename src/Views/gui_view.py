6ca097fca425f9603bf62f7d6476b38925ebdd7a
Tim <timbmoser@gmail.com>
2024-03-29 19:43:19 -0400

    Method Delete dropdowns
    Follow same process as field delete

--- src/Views/gui_view.py
+++ src/Views/gui_view.py
@@ -284,7 +284,7 @@ class GUI_View(tk.Tk):
         if len(self._class_boxes) > 0:
             # TODO: See field_options_menu above
             menu.add_command(label = "Add Method", command = self.add_method)
-            # menu.add_command(label = "Delete Method", command = self.delete_method)
+            menu.add_command(label = "Delete Method", command = self.delete_method)
             # menu.add_command(label = "Rename Method", command = self.rename_method)
         try:
             menu.tk_popup(x = self._sidebar.winfo_pointerx(), y = self._sidebar.winfo_pointery())
@@ -411,7 +411,20 @@ class GUI_View(tk.Tk):
             new_command = "add method " + class_name + " " + method
             self._commands.add(new_command)
 
-    # TODO: Inlude Delete and Rename Methods
+    def delete_method(self):
+        class_options = []
+        for cb in self._class_boxes:
+            class_options.append(cb._name)
+
+        dialog_result = Delete_Method_Dialog(self, class_options, title = "Delete Method").result
+        if dialog_result:
+            class_name, method_name = dialog_result
+            
+            new_command = "delete method " + class_name + " " + method_name
+            self._commands.add(new_command)
+
+    def rename_method(self):
+        pass
 
     def add_param(self):
         dialog_result = Add_Parameter_Dialog(self, title="Add Parameter").result
@@ -649,13 +662,15 @@ class Delete_Field_Dialog(simpledialog.Dialog):
         tk.Label(master, text = "Select Class:").grid(row = 0)
         self._class = tk.StringVar(master)
         self._class.trace("w",self.update_options)
-        self._class_select = tk.OptionMenu(master, self._class, *self._class_options).grid(row = 0, column = 1)
+        self._class_select = tk.OptionMenu(master, self._class, *self._class_options)
+        self._class_select.grid(row = 0, column = 1)
 
         # TODO: Get Fields from entered class.
-        self._fields = ['Test1', 'Test2']
+        self._fields = []
         tk.Label(master, text = "Field Name:").grid(row = 1)
         self._delete_field = tk.StringVar(master)
-        self._field_options = tk.OptionMenu(master, self._delete_field, self._fields).grid(row = 1, column = 1)
+        self._field_options = tk.OptionMenu(master, self._delete_field, self._fields)
+        self._field_options.grid(row = 1, column = 1)
 
         return self._class, self._delete_field
 
@@ -669,7 +684,7 @@ class Delete_Field_Dialog(simpledialog.Dialog):
         menu.delete(0,'end')
 
         for o in new_opts:
-            self._field_options['menu'].add_command(label=o, command=tk._setit(self._delete_field,o))
+            self._field_options['menu'].add_command(label = o, command = tk._setit(self._delete_field,o))
 
     def apply(self):
         class_name = self._class.get()
@@ -696,6 +711,44 @@ class Add_Method_Dialog(simpledialog.Dialog):
         class_name = self._class.get()
         method_name = self._method_entry.get()
         self.result = class_name, method_name
+
+class Delete_Method_Dialog(simpledialog.Dialog):
+    def __init__(self, parent, class_options:list = None, title:str = None):
+        self._class_options = class_options
+        super().__init__(parent, title=title)
+
+    def body(self, master):
+        tk.Label(master, text = "Select Class:").grid(row = 0)
+        self._class = tk.StringVar(master)
+        self._class.trace("w",self.update_options)
+        self._class_select = tk.OptionMenu(master, self._class, *self._class_options)
+        self._class_select.grid(row = 0, column = 1)
+
+        # TODO: Get Methods from entered class.
+        self._methods = []
+        tk.Label(master, text = "Method Name:").grid(row = 1)
+        self._delete_method = tk.StringVar(master)
+        self._method_options = tk.OptionMenu(master, self._delete_method, self._methods)
+        self._method_options.grid(row = 1, column = 1)
+
+        return self._class, self._delete_method
+
+    def update_options(self, *args):
+        self._delete_method.set('')
+        class_name = self._class.get()
+        # TODO: This needs to be poplulated with the methods from the class.
+        new_opts = [f'{class_name}1', f'{class_name}2', f'{class_name}3']
+        
+        menu = self._field_options['menu']
+        menu.delete(0,'end')
+
+        for o in new_opts:
+            self._field_options['menu'].add_command(label = o, command = tk._setit(self._delete_field,o))
+
+    def apply(self):
+        class_name = self._class.get()
+        field_name = self._delete_field.get()
+        self.result = class_name, field_name
 
 class Add_Parameter_Dialog(simpledialog.Dialog):
     def __init__(self, parent, title=None):
